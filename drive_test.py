a3797483f429560eb98a679e07abd07c79f957fc
Dileep <dileep.wafu@gmail.com>
2020-04-11 14:16:59 +0530

    test1

---
+++ drive_test.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaFileUpload
+import socket
+import os
+
+
+socket.setdefaulttimeout(600)  # set timeout to 10 minutes
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+my_folder_id = '16m8_vJaE--4LludRLZNSVVP86j1XrAkT'
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+TEMP_DIR = os.path.join(BASE_DIR, "temp")
+OUTPUT_DIR = os.path.join(BASE_DIR, "output")
+
+# conver sting paths..
+BASE_DIR = '/'.join(BASE_DIR.split('\\'))
+TEMP_DIR = '/'.join(TEMP_DIR.split('\\'))
+OUTPUT_DIR = '/'.join(OUTPUT_DIR.split('\\'))
+
+creds = None
+
+
+print("Executing Drive.py")
+print("Getting tokne.pickle")
+
+print(f"BASE DIR :: {BASE_DIR}")
+print(f"TEMP_DIR :: {TEMP_DIR}")
+print(f"OUTPUT_DIR :: {OUTPUT_DIR}\n\n")
+
+
+
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+       creds = pickle.load(token)
+else:
+    print("NO TOKEN >>> :(")
+
+service = build('drive', 'v3', credentials=creds)
+
+
+def upload_video_to_this_folder(folder_id, file_name):
+    file_name = file_name+'.mp4'
+    print(f"Uploading {file_name} to my_folder.. :: {folder_id}")
+    file_metadata = {
+    'name': file_name,
+    'parents': [folder_id]
+    }
+    file_path = os.path.join(OUTPUT_DIR, file_name)
+    file_path = '/'.join(file_path.split('\\'))
+    print(f"File path : {file_path}")
+    media = MediaFileUpload(file_path, mimetype='video/mp4')
+    file_res = service.files().create(body=file_metadata, media_body=media,).execute() # pylint: disable=maybe-no-member
+    print('File ID: %s' % file_res.get('id'))
+    print(f"File upload done : {file_name} :: \n {file_res}")
+    print(f"deleting file : {file_path}")
+    media =None # to close connection with file
+    # os.remove(file_path)
+
+upload_video_to_this_folder(my_folder_id, 'bramu')
